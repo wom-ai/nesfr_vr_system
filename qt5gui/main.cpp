cf8da2aed04b4f6efe62dbc7224a2a9aca5756de
Jaeyoung Lee <jylee0121@gmail.com>
2022-07-29 20:52:19 +0900

    update: run video_stream, rs2_ctrl on xterms
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- qt5gui/main.cpp
+++ qt5gui/main.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QProcess>
+#include <QDir>
+#include <QDebug>
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
+    int result = 0;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
-    return a.exec();
+    QProcess *video_streamer_process = new QProcess();
+    //video_streamer_process->setWorkingDirectory(QDir::homePath());
+    video_streamer_process->start("xterm", QStringList() << "+hold" << "-e" << "../nesfr_vr_video_streamer/build/video_stream");
+
+    QProcess *ctrl_process = new QProcess();
+    ctrl_process->start("xterm", QStringList() << "+hold" << "-e" << "../nesfr_vr_ctrl/build/rs2_vr_ctrl");
+
+    qDebug() << QDir::homePath();
+    result &= a.exec();
+
+    qDebug()<< video_streamer_process->readAllStandardOutput();
+    qDebug()<< ctrl_process->readAllStandardOutput();
+    video_streamer_process->terminate();
+    ctrl_process->terminate();
+    return result;
 }

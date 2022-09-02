a6af9e774aabf66b9f80cf41c7791cc9fe41575d
Jaeyoung Lee <jylee0121@gmail.com>
2022-09-02 20:14:11 +0900

    update: update scripts flir aravis gstreamer streaming
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- scripts/receiver_aravis_udp_streaming_left.sh
+++ scripts/receiver_aravis_udp_streaming_left.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
 
-gst-launch-1.0 -v -e udpsrc port=5000 ! 'application/x-rtp,encoding-name=H264,payload=96' ! rtph264depay ! avdec_h264 ! xvimagesink sync=0
+gst-launch-1.0 -v -e udpsrc port=5000 ! 'application/x-rtp,encoding-name=H264,payload=96' ! rtph264depay ! avdec_h264 ! textoverlay text=left valignment=top ! xvimagesink sync=0
--- scripts/receiver_aravis_udp_streaming_right.sh
+++ scripts/receiver_aravis_udp_streaming_right.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-gst-launch-1.0 -v -e udpsrc port=5001 ! 'application/x-rtp,encoding-name=H264,payload=96' ! rtph264depay ! avdec_h264 ! xvimagesink sync=0
+gst-launch-1.0 -v -e udpsrc port=5001 ! 'application/x-rtp,encoding-name=H264,payload=96' ! rtph264depay ! avdec_h264 ! textoverlay text=right valignment=top ! xvimagesink sync=0

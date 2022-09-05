77e971cb59e111fac4c0448d90848e1a981c00c2
Jaeyoung Lee <jylee0121@gmail.com>
2022-09-05 18:32:56 +0900

    update: steaming scripts
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- scripts/sender_aravis_udp_streaming_left.sh
+++ scripts/sender_aravis_udp_streaming_left.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -x
+HOST=192.168.0.4
 gst-launch-1.0 -v -e aravissrc camera-name=FLIR-1E100124E18A-0124E18A ! "video/x-bayer, format=(string)rggb, width=(int)1440, height=(int)1080, framerate=(fraction)72/1" ! bayer2rgb \
     ! videoconvert ! "video/x-raw, format=(string)RGBA" \
     ! nvvideoconvert ! "video/x-raw(memory:NVMM)" \
     ! nvv4l2h264enc maxperf-enable=1 insert-sps-pps=1 idrinterval=30 bitrate=6000000 ! h264parse ! rtph264pay mtu=65000 \
-    ! udpsink host=192.168.55.100 port=5000 sync=0 \
+    ! udpsink host=$HOST port=5000 sync=0 \

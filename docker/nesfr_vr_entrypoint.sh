553fe0aeae722030b967f5c2baba0e5b84683711
Jaeyoung Lee <jylee0121@gmail.com>
2023-01-05 21:52:01 +0900

    update: update scripts inside ros2 container
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- docker/nesfr_vr_entrypoint.sh
+++ docker/nesfr_vr_entrypoint.sh
@@ -2,10 +2,10 @@
 
 #set -x
 
-#
-# reference
-#  - https://superuser.com/questions/1539634/pulseaudio-daemon-wont-start-inside-docker
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+##
+## reference
+##  - https://superuser.com/questions/1539634/pulseaudio-daemon-wont-start-inside-docker
+#pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
 #set -e
 source /opt/ros/foxy/setup.bash
--- ros2/run_release_nesfr_vr_ros2.sh
+++ ros2/run_release_nesfr_vr_ros2.sh
@@ -4,6 +4,20 @@ echo "######################################################################"
 echo " run_release_nesfr_vr_ros2.sh"
 echo "######################################################################"
 
+echo ">>> set config"
+if [ -e "$HOME/.nesfrvr" ] && [ -d "$HOME/.nesfrvr" ]; then
+  echo "Found $HOME/.nesfrvr"
+  rm -rf $HOME/.nesfrvr/*
+else
+  echo "Make $HOME/.nesfrvr"
+  mkdir -p  $HOME/.nesfrvr
+fi
+
+mkdir -p  $HOME/.nesfrvr/configs
+ln -s /opt/ros/foxy/share/nesfr_vr_ros2/configs/hw_config_${HOSTNAME}_1280x720.json $HOME/.nesfrvr/configs/hw_config.json
+ln -s /opt/ros/foxy/share/nesfr_vr_ros2/data $HOME/.nesfrvr/data
+echo "<<< set config"
+
 pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
 ros2 run nesfr_vr_ros2 nesfr_vr_ros2
